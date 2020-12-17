d7fa47c09b404d6d570c3eec494bf3f096c4bade
yuta0801 <yuta030801@gmail.com>
2020-12-17 10:25:12 +0900

    cors support

--- main.ts
+++ main.ts
@@ -2,15 +2,24 @@ import { serve } from "https://deno.land/std@0.80.0/http/server.ts";
 
 const socket = Deno.listen({ port: 4000 });
 
+const headers = new Headers({ "Access-Control-Allow-Origin": "*" });
+
 console.log("waiting connection");
 for await (const conn of socket) {
   console.log("waiting http request");
   const server = serve({ port: 8000 });
   for await (const request of server) {
+    if (request.method === "OPTIONS") {
+      request.respond({
+        headers: new Headers({ "Access-Control-Allow-Origin": "*" }),
+      });
+      continue;
+    }
+
     console.log("piping");
     await Promise.allSettled([
       Deno.copy(request.body, conn),
-      request.respond({ body: conn }),
+      request.respond({ body: conn, headers }),
     ]);
 
     console.log("piped");
