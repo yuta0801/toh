830a9efe8deb5be725224379bdb9d52f01fc9c02
yuta0801 <yuta030801@gmail.com>
2020-12-18 17:16:06 +0900

    use 2 http request for two-way streaming

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-import { serve } from "https://deno.land/std@0.80.0/http/server.ts";
+import {
+  serve,
+  Server,
+  ServerRequest,
+} from "https://deno.land/std@0.80.0/http/server.ts";
 
 const socket = Deno.listen({ port: 4000 });
 
@@ -8,6 +12,23 @@ console.log("waiting connection");
 for await (const conn of socket) {
   console.log("waiting http request");
   const server = serve({ port: 8000 });
+
+  const handler = handle(server);
+  const upstream = await consumeRequest(handler);
+  const downstream = await consumeRequest(handler);
+
+  console.log("piping");
+  await Promise.allSettled([
+    downstream.respond({ body: conn, headers }),
+    Deno.copy(upstream.body, conn),
+  ]);
+
+  console.log("piped");
+  server.close();
+  break;
+}
+
+async function* handle(server: Server) {
   for await (const request of server) {
     if (request.method === "OPTIONS") {
       request.respond({
@@ -16,14 +37,14 @@ for await (const conn of socket) {
       continue;
     }
 
-    console.log("piping");
-    await Promise.allSettled([
-      Deno.copy(request.body, conn),
-      request.respond({ body: conn, headers }),
-    ]);
-
-    console.log("piped");
-    server.close();
-    break;
+    yield request;
   }
+}
+
+async function consumeRequest(
+  hander: AsyncGenerator<ServerRequest, void>,
+) {
+  const result = await hander.next();
+  if (result.done) throw new Error("Server exited");
+  return result.value;
 }
