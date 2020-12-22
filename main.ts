3bf2ca6d44be39502c86a4b9075f5dc8ff75a41f
yuta0801 <yuta030801@gmail.com>
2020-12-22 13:31:53 +0900

    consume http request serially
    for wasm-bindgen without streaming suppert

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import {
   Server,
   ServerRequest,
 } from "https://deno.land/std@0.80.0/http/server.ts";
+import { deferred } from "https://deno.land/std/async/mod.ts";
 
 const socket = Deno.listen({ port: 4000 });
 const server = serve({ port: 8000 });
@@ -10,18 +11,36 @@ const handler = handle(server);
 
 const headers = new Headers({ "Access-Control-Allow-Origin": "*" });
 
+class StoppableReader implements Deno.Reader {
+  private controller = deferred<null>();
+
+  constructor(private reader: Deno.Reader) {}
+
+  async read(buf: Uint8Array): Promise<number | null> {
+    return await Promise.race([
+      this.reader.read(buf),
+      this.controller,
+    ]);
+  }
+
+  stop() {
+    this.controller.resolve(null);
+  }
+}
+
 while (true) {
   console.log("waiting http request");
-  const upstream = await consumeRequest(handler);
   const downstream = await consumeRequest(handler);
-
   console.log("waiting connection");
   const conn = await socket.accept();
-
   console.log("piping");
+
+  const reader = new StoppableReader(conn);
+  setTimeout(() => reader.stop(), 1000);
+
   await Promise.allSettled([
-    downstream.respond({ body: conn, headers }),
-    Deno.copy(upstream.body, conn),
+    downstream.respond({ body: reader, headers }),
+    consumeRequest(handler).then((upstream) => Deno.copy(upstream.body, conn)),
   ]);
   console.log("piped");
 }
